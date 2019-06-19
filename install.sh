e7d59800665188b1f12ecafdd417d42595b48c7c
liberodark <liberodark@gmail.com>
2019-06-19 19:54:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# About: Install Ryzen Stabilizator automatically
+# Author: liberodark
+# License: GNU GPLv3
+
+version="0.0.1"
+
+echo "Welcome on Ryzen Stabilizator Install Script $version"
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+distribution=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
+
+install_ryzen_stabilizator(){
+      git clone https://github.com/qrwteyrutiyoup/ryzen-stabilizator.git &> /dev/null
+      pushd ryzen-stabilizator/ || exit
+      export GOPATH=./go
+      go get &> /dev/null
+      go build &> /dev/null
+      mv ryzen-stabilizator.conf /etc/modules-load.d/ryzen-stabilizator.conf
+      chmod 0644 /etc/modules-load.d/ryzen-stabilizator.conf
+      mkdir /usr/share/licenses/ryzen_stabilizator/
+      mv LICENSE /usr/share/licenses/ryzen_stabilizator/LICENSE
+      mv ./contrib/systemd/ryzen* /usr/lib/systemd/system/
+      mkdir /etc/ryzen-stabilizator/
+      mv ./contrib/settings* /etc/ryzen-stabilizator/settings.toml
+      chmod 0644 /etc/ryzen-stabilizator/settings.toml
+      mv ./ryzen-stabilizator-master /usr/bin/ryzen-stabilizator
+      chmod 0755 /usr/bin/ryzen-stabilizator
+      popd || exit
+}
+
+deps_install(){
+echo "Install Ryzen Stabilizator ($distribution)"
+
+  # Check OS & git / go
+
+  if ! command -v nrpe &> /dev/null; then
+
+    if [[ "$distribution" = CentOS || "$distribution" = CentOS || "$distribution" = Red\ Hat || "$distribution" = Fedora || "$distribution" = Suse || "$distribution" = Oracle ]]; then
+      yum install -y git go &> /dev/null
+
+      install_ryzen_stabilizator || exit
+    
+    elif [[ "$distribution" = Debian || "$distribution" = Ubuntu || "$distribution" = Deepin ]]; then
+      apt-get update &> /dev/null
+      apt-get install -y git go --force-yes &> /dev/null
+    
+      install_ryzen_stabilizator || exit
+      
+    elif [[ "$distribution" = Manjaro || "$distribution" = Arch\ Linux ]]; then
+      pacman -S git go --noconfirm &> /dev/null
+    
+      install_ryzen_stabilizator || exit
+
+    fi
+fi
+}
